87ccec3a79d59174f7704cb95332494a7129b3b7
James Turello and Katarina Tuttle <james@>
2016-07-07 10:24:21 -0700

    modify initialize methods and specs to take an attributes hash as parameter rather than individual params

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,23 +1,15 @@
 class Vehicle
+  attr_reader(:make, :model, :year, :color, :engine_size, :number_of_doors, :id)
   @@vehicles = []
 
-  define_method(:initialize) do |make, model, year|
-    @make = make
-    @model = model
-    @year = year
+  define_method(:initialize) do |attributes|
+    @make = attributes[:make]
+    @model = attributes[:model]
+    @year = attributes[:year].to_i
+    @color = attributes[:color]
+    @engine_size= attributes[:engine_size]
+    @number_of_doors = attributes[:number_of_doors]
     @id = @@vehicles.length().+(1)
-  end
-
-  define_method(:make) do
-    @make
-  end
-
-  define_method(:model) do
-    @model
-  end
-
-  define_method(:year) do
-    @year
   end
 
   define_singleton_method(:all) do
@@ -42,9 +34,6 @@ class Vehicle
     american_cars.include?(@make).&(self.age().<=(15))
   end
 
-  define_method(:id) do
-    @id 
-  end
 
   define_singleton_method(:find) do |identification|
     found_vehicle = nil
